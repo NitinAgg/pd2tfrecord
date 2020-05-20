82456e5c18144cf01802a649bad92542eceddbeb
Nitin Aggarwal <nitin.agg1909@gmail.com>
2020-05-19 20:55:06 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name="pd2tfrecord",
+    packages=["pd2tfrecord"],
+    version="0.1",
+    license="MIT",
+    description="Library to convert pandas data frame to tfrecord and vice versa",
+    author="Nitin Aggarwal",
+    author_email="nitin.agg1909@gmail.com",
+    url="https://github.com/NitinAgg/pd2tfrecord",
+    download_url="https://github.com/NitinAgg/pd2tfrecord/archive/v_01.tar.gz",
+    keywords=["tensorflow", "tfrecord", "pandas", "dataframe", "tf.example"],
+    install_requires=["tensorflow>=2.0", "pandas"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
