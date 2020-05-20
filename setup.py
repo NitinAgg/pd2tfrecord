1215110f63d87b590eca3805d979b06584530f18
Nitin Aggarwal <nitin.agg1909@gmail.com>
2020-05-19 21:54:16 -0700

    try-publish

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="pd2tfrecord",
     py_modules=["pd2tfrecord"],
-    version="0.1",
+    version="0.2",
     license="MIT",
     description="Library to convert pandas data frame to tfrecord and vice versa",
     author="Nitin Aggarwal",
     author_email="nitin.agg1909@gmail.com",
     url="https://github.com/NitinAgg/pd2tfrecord",
-    download_url="https://github.com/NitinAgg/pd2tfrecord/archive/v_01.tar.gz",
-    keywords=["tensorflow", "tfrecord", "pandas", "dataframe", "tf.example"],
+    download_url="https://github.com/NitinAgg/pd2tfrecord/archive/v_02.tar.gz",
+    keywords="tensorflow tfrecord pandas dataframe tf.example",
     install_requires=["tensorflow>=2.0", "pandas"],
     classifiers=[
         "Development Status :: 4 - Beta",
